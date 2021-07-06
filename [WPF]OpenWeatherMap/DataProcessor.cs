6c536b0990bba1c5f63826efe4d85e37065570e1
BorMooh <bor.mohoric2@gmail.com>
2021-07-06 21:19:44 +0200

    simple output, better styling, adding DataProcessor class

---
+++ [WPF]OpenWeatherMap/DataProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using _WPF_OpenWeatherMap.Model;
+using Newtonsoft.Json;
+
+namespace _WPF_OpenWeatherMap
+{
+    public class DataProcessor
+    {
+        /// <summary>
+        /// Metoda, s katero pridobimo podatke o vnešenem mestu. Kot parameter vnesemo določeno mesto 
+        /// </summary>
+        /// <param name="city"></param>
+        public static WeatherModel.Root GetWeather(string city = "Zelezniki")
+        {
+            //Z web-clientom pridobimo vse podatke iz določenega URL naslova, ki nam vrne JSON izpis. 
+            using (WebClient client = new WebClient())
+            {
+                //URL 
+                string url = string.Format($"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=16f364f0a3b530faec39488f8a34aab3");
+
+
+                //Z WebClientom prenesemo podatke iz našega URL-ja
+                var json = client.DownloadString(url);
+
+                //Rezultat deserializiramo z JsonConverter, in to shranimo kot tip WeatherModel
+                var result = JsonConvert.DeserializeObject<WeatherModel.Root>(json);
+                WeatherModel.Root output = result;
+
+
+                //Metoda vrne tip WeatherModel z vsemi podatki
+                return output;
+            }
+
+
+        }
+
+        //Metoda za pridobivanje ikone 
+        public static string GetIcon(string id)
+        {
+            string fullpath = "http://openweathermap.org/img/wn/";
+            fullpath += id + ".png";
+
+            return fullpath;
+        }
+    }
+
+
+
+}
--- [WPF]OpenWeatherMap/MainWindow.xaml.cs
+++ [WPF]OpenWeatherMap/MainWindow.xaml.cs
@@ -18,45 +18,71 @@ using Newtonsoft.Json;
 
 namespace _WPF_OpenWeatherMap
 {
+    #region README
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///  TODO
+    ///     UTF-8 PODPORA ZA ŠUMNIKE
+    ///     OPTIMIZACIJA KODE ZA DODAJANJE SLIKE/IKONE
+    ///     COMBOBOX TEXT PLACEMENT - TEKST NI PORAVNAN
+    ///     CITY LIST - DA IMAMO VSA MESTA NOTRI V COMBOBOXU
+    ///     SLIKE SO LOWRES???
+    ///     VEČ POLJ
     /// </summary>
     /// 
-    //API: `http://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&appid=16f364f0a3b530faec39488f8a34aab3`;
+
+    //API: `http://api.openweathermap.org/data/2.5/weather?q=zelezniki&appid=16f364f0a3b530faec39488f8a34aab3`;
+    //ikona: http://openweathermap.org/img/wn/10d@2x.png
+    #endregion
+
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
-            getWeather();
-            
         }
 
-
-        /// <summary>
-        /// Metoda, s katero pridobimo podatke o vnešenem mestu. Kot parameter vnesemo določeno mesto 
-        /// </summary>
-        /// <param name="city"></param>
-        public  void getWeather(string city = "Zelezniki")
+        //Ob kliku na gumb "Search"
+        static WeatherModel.Root podatki;
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-
-            //Z web-clientom pridobimo vse podatke iz določenega URL naslova, ki nam vrne JSON izpis. 
-            using(WebClient client = new WebClient())
+            //Podatke probamo pridobiti - če to ne deluje, nam program izpiše da je prišlo do napake
+            try
             {
-                //URL 
-                string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q=zelezniki&appid=16f364f0a3b530faec39488f8a34aab3");
+                podatki = new WeatherModel.Root();
+                podatki = DataProcessor.GetWeather(inputField.Text); // DODAJ PARAMETER - MESTO
 
-                //Z WebClientom prenesemo podatke iz našega URL-ja
-                var json = client.DownloadString(url);
-                
-                //Rezultat deserializiramo z JsonConverter, in to shranimo kot tip WeatherModel
-                var result = JsonConvert.DeserializeObject<WeatherModel.Root>(json);
-                WeatherModel.Root output = result;
-
-                //IZPIS
-                labelTest.Content = output.weather[0].description;
+                InsertData();
             }
-            
+            catch
+            {
+                MessageBox.Show("Prišlo je do napake!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
+
+        #region Vnos podatkov v polja
+        /// <summary>
+        /// Metoda s katero se vsi podatki vnesejo v pravilna polja
+        /// </summary>
+        private void InsertData()
+        {
+            //Testno ime
+            labelTest.Content = podatki.name;
+
+            //Za dodajanje slike
+            Uri weatherUri = new Uri(DataProcessor.GetIcon(podatki.weather[0].icon.ToString()), UriKind.Absolute);
+            BitmapImage bmi = new BitmapImage();
+            bmi.BeginInit();
+            bmi.UriSource = weatherUri;
+            bmi.EndInit();
+            weatherIcon.Source = bmi;
+
+            weatherDescription.Content = podatki.weather[0].description;
+
+
+            //Temperatura
+            temperatureLabel.Content = Math.Floor(podatki.main.temp - 273.15) + "c";
+        }
+        #endregion
     }
 }
